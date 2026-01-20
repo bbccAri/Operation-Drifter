d21086f97203cad6f41733540494b7256391338d
bbcc <boxybirdmc@gmail.com>
2026-01-20 16:43:23 -0500

    remove physics from exploded debris

--- orbit_debris.gd
+++ orbit_debris.gd
@@ -16,6 +16,7 @@ var count_up_timer: float = 0.0
 var expiration_date: float = 20.0
 var scrap_rarity: DebrisSpawner.ScrapRarity = DebrisSpawner.ScrapRarity.Materials
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionObject2D = $Collision
 var grabbed: bool = false
 var exploding: bool = false
 @onready var explode_particles: GPUParticles2D = $DebrisExplodeParticles
@@ -84,6 +85,8 @@ func explode():
 	if not exploding:
 		exploding = true
 		sprite.visible = false
+		collision.disable_mode = CollisionObject2D.DISABLE_MODE_REMOVE
+		collision.process_mode = Node.PROCESS_MODE_DISABLED
 		if grabbed:
 			player.drop_object()
 		explode_particles.restart()
