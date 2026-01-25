8cdd0298dfcc0eeb5b04b7b6b0716f58102e74b1
bbcc <boxybirdmc@gmail.com>
2026-01-25 18:58:57 -0500

    update player sprites; start dialogic work

--- shop.gd
+++ shop.gd
@@ -3,16 +3,27 @@ extends Node2D
 var player: Player
 var player_in_range: bool = false
 @onready var label: RichTextLabel = $RichTextLabel
+var done_tutorial: bool = false
 
-func enter_tutorial(body: Player):
-	player = body
+func enter_tutorial():
+	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		if player == null:
-			enter_tutorial(body)
+			player = body
 		player_in_range = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Player:
 		player_in_range = false
+
+func _process(_delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("Interact"):
+		open_shop()
+
+func open_shop():
+	if !done_tutorial:
+		enter_tutorial()
+	else:
+		pass
